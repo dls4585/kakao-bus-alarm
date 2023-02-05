36460d439061dc2b900bf718f1e25bc9554c7596
dls4585 <dls4585@naver.com>
2023-02-05 19:01:57 +0900

    chore: remove spring dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.8"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    kotlin("jvm") version "1.6.10"
 }
 
-group = "com.tong-geun-bus"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+group = "me.inchan"
+version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("org.java-websocket:Java-WebSocket:1.5.3")
-	implementation("org.json:json:20220924")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
-rootProject.name = "alarm"
+
+rootProject.name = "kakao-bus-alarm"
